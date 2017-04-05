01dbc4f4a8b5d25be7c8fa9ddea622225144f19e
Endill <brainvlad@gmail.com>
2017-04-05 13:07:34 +0300

    bpd-project.cpp: add "Users" class testing

--- bpd-project/bpd-project.cpp
+++ bpd-project/bpd-project.cpp
@@ -1,11 +1,26 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 // bpd-project.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
 
+#include <iostream>
+#include "Subscriber.h"
+#include "Users.h"
+
+using namespace std;
 
 int main()
 {
-    return 0;
+    try {
+        users.add("admin", "admin");
+        users.getUser("admin").setRole("admin");
+        users.getUser("user");
+    } catch (string errorMessage) {
+        cout << errorMessage << endl;
+    }
+	return 0;
 }
 
