76f2030f2e671616b17b92d3cfb5192e0a00bc1e
Endill <brainvlad@gmail.com>
2017-04-05 13:06:08 +0300

    add "Subscriber" class implementation

--- bpd-project/Subscriber.cpp
+++ bpd-project/Subscriber.cpp
@@ -1,35 +1,30 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
+#include "stdafx.h"
+
 #include <string>
+
+#include "Subscriber.h"
 
 using namespace std;
 
-class Subscriber {
-public:
-	Subscriber(string          fullName,
-			   long long       number,
-			   const long long year,
-			   string          plan     )
-				   : fullName_(fullName),
-					 number_  (number),
-					 year_    (year),
-					 plan_    (plan)     {}
+Subscriber::Subscriber(const string    fullName,
+			           const long long number,
+			           const long long year,
+			           const string    plan) :
+			           fullName_(fullName),
+			           number_  (number),
+			           year_    (year),
+			           plan_    (plan) {}
 
-	string getFullName()				{ return fullName_;     }
-	void   setFullName(string fullName) { fullName_ = fullName; }
+string   Subscriber::getFullName() const			{ return fullName_;     }
+//void     Subscriber::setFullName(string fullName) { fullName_ = fullName; }
 
-	long long getNumber()				  { return number_;   }
-	void	  setNumber(long long number) { number_ = number; }
+long long Subscriber::getNumber() const			  { return number_;   }
+void	  Subscriber::setNumber(long long number) { number_ = number; }
 
-	const long long getYear() { return year_; }
+long long Subscriber::getYear() const { return year_; }
 
-	string getPlan()			{ return plan_; }
-	void   setPlan(string plan) { plan_ = plan; }
-
-private:
-	string          fullName_;
-	long long       number_;
-	const long long year_;
-	string          plan_;
-};
+string    Subscriber::getPlan() const		{ return plan_; }
+void      Subscriber::setPlan(string plan) { plan_ = plan; }
