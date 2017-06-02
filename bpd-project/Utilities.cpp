227b3ba39fcf7871bf34bd0b35f3cdef6a048639
Endill <brainvlad@gmail.com>
2017-06-03 01:14:25 +0300

    Utilities: add SetStdinEcho()

---
+++ bpd-project/Utilities.cpp
@@ -0,0 +1,19 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include "stdafx.h"
+#include "Utilities.h"
+
+#include <windows.h>
+
+using namespace std;
+
+void SetStdinEcho(bool enable) {
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hStdin, &mode);
+    if (enable)
+        mode |= ENABLE_ECHO_INPUT;
+    else
+        mode &= ENABLE_ECHO_INPUT;
+    SetConsoleMode(hStdin, mode);
+}
